576e6b481f6f33575d620189976cd4ad2274df72
NumAniCloud <num.ani.cloud@gmail.com>
2020-10-15 13:14:00 +0900

    ディレクトリを整理

--- RpgGenerator.Generator/Resources.Designer.cs
+++ RpgGenerator.Generator/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RpgGenerator {
+namespace RpgGenerator.Generator {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace RpgGenerator {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RpgGenerator.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RpgGenerator.Generator.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
